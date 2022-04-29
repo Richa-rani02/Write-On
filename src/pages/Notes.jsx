cb83436c4e66b23bc47ad6885ac45c8abab17d71
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-04-29 21:41:18 +0530

    refactor:improved notes UI

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,18 +1,19 @@
-import { Header, Sidebar,Note,LabelModal,TextEditor} from "../components";
-import {IoMdAdd} from "react-icons/io";
+import { Header, Sidebar, Note, LabelModal, TextEditor } from "../components";
+import { IoMdAdd } from "react-icons/io";
 import { useGlobalContext } from "../context/global-context";
-import {useNotes} from "../context/notes-context";
+import { useNotes } from "../context/notes-context";
+import { MdTune } from "react-icons/md";
 export const Notes = () => {
 
-const {tooglenotesModal}=useGlobalContext();
-const {notesState:{notesList},setNotes,noteInput}=useNotes();
+    const { tooglenotesModal } = useGlobalContext();
+    const { notesState: { notesList }, setNotes, noteInput } = useNotes();
 
     return (
         <div className="notes bg-[#FAFAFA] h-full w-screen">
             <Header />
             <Sidebar />
             <main className="px-3 py-2 lg:ml-64 ml-0 md:ml-64 relative">
-                <div class="flex flex-wrap lg:justify-between md:justify-between justify-center  items-center gap-4 mt-6 px-7 py-2 ">
+                {/* <div class="flex flex-wrap lg:justify-between md:justify-between justify-center  items-center gap-4 mt-6 px-7 py-2 ">
                     <div
                         class="flex justify-center order-1"
                     >
@@ -41,19 +42,45 @@ const {notesState:{notesList},setNotes,noteInput}=useNotes();
                             </select>
                         </div>
                     </div>
+                </div> */}
+                
+                <div class="flex flex-wrap justify-around mt-8 items-center gap-6">
+                <div class="flex">
+                        <button type="button" class="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 relative" onClick={(e)=>{
+                            tooglenotesModal(e);
+                            setNotes(noteInput)
+                            }}>
+                           Add Notes
+                            <IoMdAdd size={22} className="ml-2"/>
+                        </button>
+                    </div>
+                    
+                    <div class="flex pr-6">
+                        <input type="search" placeholder="search here..." className="p-2.5 w-72 xl:w-96 border-b-2 border-cyan-500 bg-[#f5f3ff] focus:outline-none"
+                        />
+                        <button type="button" class="text-white bg-cyan-500 hover:bg-cyan-800 focus:outline-none font-medium text-sm py-2.5 px-7 text-center inline-flex items-center dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
+                            <MdTune size={24} />
+                        </button>
+                    </div>
+                    
                 </div>
+
+
+
+
+
                 <div className="notes-list-container my-12 mx-5  p-3 ">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-                {notesList?.map((note)=>(
-                   <Note key={note.id} notes={note}/> 
-                ))}
-                </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                        {notesList?.map((note) => (
+                            <Note key={note.id} notes={note} />
+                        ))}
+                    </div>
                 </div>
             </main>
-            <LabelModal/>
-            <TextEditor/>
-           
+            <LabelModal />
+            <TextEditor />
+
         </div>
-         
+
     )
 }

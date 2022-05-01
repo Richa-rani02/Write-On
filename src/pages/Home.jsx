fcb36c9638d0c8b44853dbcbab512f3aa014696f
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2022-05-01 17:57:19 +0530

    feat:added homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-
+import {Link} from "react-router-dom";
 export const Home = () => {
+  //  let navigate=useNavigate();
     return (
-        <div className="home">
-
-            <h1>This is home page</h1>
+        <div className="home flex w-screen h-screen flex-wrap">
+            <div className="p-5 w-full lg:w-3/6 flex flex-col items-center justify-center text-center gap-6">
+            <h1 className="text-5xl lg:text-5xl font-extrabold text-cyan-500 font-Sail tracking-wider">Write On</h1>
+            <h1 className="text-lg font-medium text-purpleprimary tracking-wider ">Manage your daily tasks and work flow in a modern way!! </h1>
+            <Link to="/notes" type="button" class=" mt-5 text-white bg-gradient-to-r from-cyan-700 to-blue-500 hover:bg-gradient-to-bl focus:outline-none  font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2">Start Writing ...</Link>
+            </div>
+            <div className="p-5 w-full lg:w-3/6 flex items-center justify-center text-center">
+            <img src="../Assets/notesback.svg" className="w-3/5 md:w-2/6 lg:w-10/12">
+            </img>
+            </div>
         </div>
     )
 }
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -13,36 +13,6 @@ export const Notes = () => {
             <Header />
             <Sidebar />
             <main className="px-3 py-2 lg:ml-64 ml-0 md:ml-64 relative">
-                {/* <div class="flex flex-wrap lg:justify-between md:justify-between justify-center  items-center gap-4 mt-6 px-7 py-2 ">
-                    <div
-                        class="flex justify-center order-1"
-                    >
-                        <input type="search" placeholder="search here..." className="p-2 w-40 xl:w-72 border-b-2 border-cyan-500 bg-[#f5f3ff] rounded focus:outline-none"
-                        />
-                    </div>
-                    <div
-                        class="flex justify-center order-2 "
-                    >
-                        <button type="button" class="text-white mt-8 lg:mt-0  bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 relative" onClick={(e)=>{
-                            tooglenotesModal(e);
-                            setNotes(noteInput)
-                            }}>
-                           Add Notes
-                            <IoMdAdd size={22} className="ml-2"/>
-                        </button>
-                    </div>
-                    <div class="flex justify-center  xl:order-3 sm:order-4">
-                        <div class="w-auto">
-                            <select class="form-select  block w-full px-3 py-1.5 text-base font-normaltext-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded m-0
-                           focus:text-gray-700 focus:bg-white focus:border-cyan-600 focus:outline-none" aria-label="Default select example">
-                                <option selected>Select Priority</option>
-                                <option value="1">High</option>
-                                <option value="2">Medium</option>
-                                <option value="3">Low</option>
-                            </select>
-                        </div>
-                    </div>
-                </div> */}
                 
                 <div class="flex flex-wrap justify-around mt-8 items-center gap-6">
                 <div class="flex">
